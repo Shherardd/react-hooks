b2840112969efcf2e5a56f5fec79eafbd4ed4648
Luis Gerardo <gerardwjones@gmail.com>
2020-11-28 20:16:21 -0600

    useRef

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useMemo } from 'react'
+import React, { useState, useEffect, useReducer, useMemo, useRef } from 'react'
 import s from './characters.module.scss'
 
 const initialState = {
@@ -20,6 +20,7 @@ const Characters = () => {
     const [characters, setCharacters] = useState([])
     const [favorites, dispatch] = useReducer(favoriteReducer, initialState)
     const [search, setSearch] = useState('')
+    const searchInput = useRef(null)
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
         .then(response =>response.json())
@@ -33,8 +34,8 @@ const Characters = () => {
         dispatch({type: 'ADD_TO_FAVORITE', payload: favorite})
     }
 
-    const handleSearch = (event) => {
-        setSearch(event.target.value)
+    const handleSearch = () => {
+        setSearch(searchInput.current.value)
     }
 
     //const filteredUsers = characters.filter(user => {
@@ -48,7 +49,7 @@ const Characters = () => {
     ,[characters, search])
     return (
         <div className={`${s.container} ${s.dark}`}>
-            <input type="text" value={search} onChange={handleSearch}/>
+            <input type="text" value={search} ref={searchInput} onChange={handleSearch}/>
             <div className={s.characters}>
                 
                     {favorites.favorites.map( fav => (
